c8c5ffb9328af33a5ab00efa19414ce29b7bb827
CrazyCat777waos <tacosalpaator65@gmail.com>
2026-04-18 23:29:41 -0600

    Tailwind funcionando, y listo para probar estilos. Modificacion de layout

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+﻿export default {
+    content: [
+        "./Components/**/*.{razor,html}",
+        "./Pages/**/*.{razor,html}",
+        "./Shared/**/*.{razor,html}",
+        "./**/*.cshtml",
+        "./wwwroot/index.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+module.exports = {
+    content: ["./**/*.razor", "./**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                retro: "#0e0e0e",
+                panel: "#161616",
+                line: "#d4d4d4",
+                accent: "#cfcfcf"
+            },
+            fontFamily: {
+                pixel: ["'Press Start 2P'", "monospace"],
+                retro: ["'Share Tech Mono'", "monospace"]
+            },
+            boxShadow: {
+                retro: "0 0 0 1px #d4d4d4, inset 0 0 0 1px #d4d4d4"
+            }
+        }
+    },
+    plugins: []
+}
